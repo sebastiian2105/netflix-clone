3ea4e6a3dacb6cee8034df059765f2916a785487
Sebastian Salcedo <sebastiian2105@hotmail.com>
2022-06-24 19:13:59 -0500

    Primer Commit - Anexo Lista de Categorias

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Tmdb from './Tmdb'
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+export default () => {
+
+    const [movieList, setMovieList] = useState([]);
+    const [featuredData, setFeaturedData] = useState(null);
+
+    useEffect(() => {
+        const LoadAll = async () => {
+            // Total List of content
+            let list = await Tmdb.getHomeList();
+            setMovieList(list);
+
+            // add Featured
+            let originals = list.filter(i=>i.slug === 'originals');
+            let randomChose = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+            let chosen = originals[0].items.results[randomChose];
+
+            console.log(chosen)
+        }
+
+        LoadAll();
+
+    }, [])
+
+    return (
+        <div className='page'>
+
+            {featuredData &&
+                <FeaturedMovie item={featuredData} />
+            }
+
+            <section className='lists'>
+                {movieList.map((item, key)=>(
+                    <MovieRow key={key} title={item.title} items={item.items} />
+                ))}
+            </section>
+        </div>
+    )
 }
-
-export default App;
---
+++ src/Tmdb.js
@@ -0,0 +1,69 @@
+const API_KEY = 'fc5495d185a263da54750c0c95f9edaa';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+/*
+Netflix
+
+Originals
+Trending
+Top rated
+
+Action
+Comedy
+Horror
+Romance
+Drama
+*/
+
+const basicFetch = async (endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
+    return json;
+}
+
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'originals',
+                title: 'Originales de Netflix',
+                items: await basicFetch(`/discover/tv?with_network=213&language=es&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'trending',
+                title: 'Recomendados para ti',
+                items: await basicFetch(`/trending/all/week?language=es&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'toprated',
+                title: 'Mejor Calificados',
+                items: await basicFetch(`/movie/top_rated?language=es&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'action',
+                title: 'Acción',
+                items: await basicFetch(`/discover/movie?with_genres=28&language=es&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'comedy',
+                title: 'Comedia',
+                items: await basicFetch(`/discover/movie?with_genres=35&language=es&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'horror',
+                title: 'Terror',
+                items: await basicFetch(`/discover/movie?with_genres=27&language=es&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=es&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'drama',
+                title: 'Drama',
+                items: await basicFetch(`/discover/movie?with_genres=18&language=es&api_key=${API_KEY}`)
+            },
+        ]
+    }
+}
